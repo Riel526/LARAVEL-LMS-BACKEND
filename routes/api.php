adc808ff9ee7040894c56f1d9ec253967852ee22
Riel526 <gabpadolina05@gmail.com>
2026-05-03 12:26:45 +0800

    (fix) - used auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\ScheduleController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,23 +17,31 @@ Route::prefix('auth')->group(function() {
     Route::post('register', [AuthController::class, 'register']);
 });
 
-Route::prefix('students')->group(function() {
-    Route::get('/get-students', [StudentController::class, 'index']);
-    Route::post('/add-student', [StudentController::class, 'store']);
-    Route::put('/update-student/{id}', [StudentController::class, 'update']);
-    Route::delete('/delete-student/{id}', [StudentController::class, 'delete']);
-});
 
-Route::prefix('subjects')->group(function() {
-    Route::get('/get-subjects', [SubjectController::class, 'index']);
-    Route::post('/add-subject', [SubjectController::class, 'store']);
-    Route::put('/update-subject/{id}', [SubjectController::class, 'update']);
-    Route::delete('/delete-subject/{id}', [SubjectController::class, 'delete']);
-});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('students')->group(function() {
+        Route::get('/get-students', [StudentController::class, 'index']);
+        Route::post('/add-student', [StudentController::class, 'store']);
+        Route::put('/update-student/{id}', [StudentController::class, 'update']);
+        Route::delete('/delete-student/{id}', [StudentController::class, 'delete']);
+    });
 
-Route::prefix('teachers')->group(function() {
-    Route::get('/get-teachers', [TeacherController::class, 'index']);
-    Route::post('/add-teacher', [TeacherController::class, 'store']);
-    Route::put('/update-teacher/{id}', [TeacherController::class, 'update']);
-    Route::delete('/delete-teacher/{id}', [TeacherController::class, 'delete']);
+    Route::prefix('subjects')->group(function() {
+        Route::get('/get-subjects', [SubjectController::class, 'index']);
+        Route::post('/add-subject', [SubjectController::class, 'store']);
+        Route::put('/update-subject/{id}', [SubjectController::class, 'update']);
+        Route::delete('/delete-subject/{id}', [SubjectController::class, 'delete']);
+    });
+
+    Route::prefix('teachers')->group(function() {
+        Route::get('/get-teachers', [TeacherController::class, 'index']);
+        Route::post('/add-teacher', [TeacherController::class, 'store']);
+        Route::put('/update-teacher/{id}', [TeacherController::class, 'update']);
+        Route::delete('/delete-teacher/{id}', [TeacherController::class, 'delete']);
+    });
+
+
+    Route::prefix('schedule')->group(function() {
+        Route::get('/get-schedule', [ScheduleController::class, 'index']);
+    });
 });
