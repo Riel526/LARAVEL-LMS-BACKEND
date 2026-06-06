119fba27296e82866d5de201c6456cfcd56c4708
Riel526 <gabpadolina05@gmail.com>
2026-06-07 01:18:14 +0800

    (chore) - host and port for websocket

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ php artisan migrate --force
 
 # Start Reverb in the background!
 # The '--host=0.0.0.0' lets Render route public traffic into it.
-php artisan reverb:start --host=0.0.0.0 --port=8080 &
+php artisan reverb:start --host=127.0.0.1 --port=8080 &
 
 # Start the main web server processes
 service nginx start
